8a2a553688b514a93589dc817d71e30cfdf23323
Rohith1972 <rohith197277@gmail.com>
2026-07-02 12:05:02 +0530

    Time: 2 ms (96.11%) | Memory: 45.3 MB (95.82%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,10 +1,10 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
-        combination(0,candidates,target,0,new ArrayList<>(),res);
+        combination(0,candidates,target,new ArrayList<>(),res);
         return res;
     }
-    void combination(int index,int[] candidates,int target,int sum,ArrayList<Integer> ds,List<List<Integer>> res){
+    void combination(int index,int[] candidates,int target,ArrayList<Integer> ds,List<List<Integer>> res){
         if(index == candidates.length){
             if(target == 0){
                 res.add(new ArrayList<>(ds));
@@ -13,9 +13,9 @@ class Solution {
         }
         if(candidates[index]<=target){
             ds.add(candidates[index]);
-            combination(index,candidates,target-candidates[index],sum+candidates[index],ds,res);
+            combination(index,candidates,target-candidates[index],ds,res);
             ds.remove(ds.size()-1);
         }
-        combination(index+1,candidates,target,sum,ds,res);
+        combination(index+1,candidates,target,ds,res);
     }
 }
