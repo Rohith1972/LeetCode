44debc5209cf56c88fd3da99f5fa80a22c9a4b2b
Rohith1972 <rohith197277@gmail.com>
2026-07-02 21:02:38 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (90.14%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,23 +1,27 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        boolean freq[] = new boolean[nums.length];
         List<List<Integer>> res = new ArrayList<>();
-        recurPermute(nums,new ArrayList<>(),res,freq);
+        recurPermute(0,nums,res);
         return res;
     }
-    void recurPermute(int[] nums,List<Integer> ds,List<List<Integer>> res,boolean[] freq){
-        if(ds.size() == nums.length){
-            res.add(new ArrayList<>(ds));
+    void recurPermute(int index,int[] nums,List<List<Integer>> res){
+        if(index == nums.length){
+            ArrayList<Integer> ds = new ArrayList<>();
+            for(int i=0;i<nums.length;i++){
+                ds.add(nums[i]);
+            }
+            res.add(ds);
             return;
         }
-        for(int i=0;i<nums.length;i++){
-            if(!freq[i]){
-                ds.add(nums[i]);
-                freq[i]=true;
-                recurPermute(nums,ds,res,freq);
-                ds.remove(ds.size()-1);
-                freq[i]=false;
-            }
+        for(int i=index;i<nums.length;i++){
+            swap(index,i,nums);
+            recurPermute(index+1,nums,res);
+            swap(index,i,nums);
         }
+    }
+    void swap(int i,int j,int[] nums){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
