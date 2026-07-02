3387cc3e186497e013f81a10a317ed093e9b8fc0
Rohith1972 <rohith197277@gmail.com>
2026-07-02 16:22:37 +0530

    Time: 1 ms (62.67%) | Memory: 43.7 MB (5.34%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,17 +1,62 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        Arrays.sort(strs);
-        String s1 = strs[0];
-        String s2 = strs[strs.length-1];
-        int i=0,j=0;
-        while(i<s1.length()&&j<s2.length()){
-            if(s1.charAt(i)==s2.charAt(i)){
-                i++;
-                j++;
-            }else{
+        Trie trie = new Trie();
+        for(String s : strs){
+            if(s.equals(""))
+                return new String("");
+            trie.insert(s);
+        }
+        String prefix = trie.commonPrefix();
+        return prefix;
+    }
+}
+class TrieNode{
+    TrieNode child[];
+    boolean isLeaf;
+    int count;
+    TrieNode(){
+        child = new TrieNode[26];
+        isLeaf = false;
+        count = 0;
+    }
+}
+class Trie {
+    TrieNode root;
+    public Trie() {
+        root = new TrieNode();
+    }
+    
+    public void insert(String word) {
+        TrieNode curr = root;
+        for(char ch : word.toCharArray()){
+            if(curr.child[ch-'a']==null){
+                curr.child[ch-'a'] = new TrieNode();
+            }
+            curr.child[ch-'a'].count++;
+            curr = curr.child[ch-'a'];
+        }
+        curr.isLeaf = true;
+    }
+    String commonPrefix(){
+        TrieNode curr = root;
+        StringBuilder res = new StringBuilder();
+        while(true){
+            int count = 0;
+            int nextIndex = -1;
+            for(int i=0;i<26;i++){
+                if(curr.child[i]!=null){
+                    count++;
+                    nextIndex = i;
+                }
+            }
+            if(count != 1){
                 break;
             }
+            curr = curr.child[nextIndex];
+            res.append((char)('a'+nextIndex));
+            if(curr.isLeaf)
+                break;
         }
-        return s1.substring(0,i);
+        return res.toString();
     }
 }
