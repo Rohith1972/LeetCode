8e7ffbd316a419211683d369535fa5579764db87
Rohith1972 <rohith197277@gmail.com>
2026-07-06 22:26:40 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (22.20%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n == 1)
+            return nums[0];
+        int[] nums1 = new int[n-1];
+        int[] nums2 = new int[n-1];
+        for(int i=0;i<n-1;i++){
+            nums1[i] = nums[i];
+        }
+        for(int i=0;i<n-1;i++){
+            nums2[i] = nums[i+1]; 
+        }
+        return Math.max(rob1(nums1),rob1(nums2));
+    }
+    public int rob1(int[] nums) {
+        int prev = nums[0];
+        int prev2 = 0;
+        for(int i=1;i<nums.length;i++){
+            int pick = nums[i];
+            if(i-1>0)
+                pick+=prev2;
+            int notPick = prev;
+            int curri = Math.max(pick,notPick);
+            prev2 = prev;
+            prev = curri;
+        }
+        return prev;
+    }
+}
