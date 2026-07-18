f0b7783bd2727737a8bb61a481b5aa719a9b3b79
Rohith1972 <rohith197277@gmail.com>
2026-07-18 14:20:29 +0530

    Time: 49 ms (50.61%) | Memory: 46.8 MB (64.95%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -7,35 +7,23 @@ class Solution {
         }
         if(totSum%2!=0)
             return false;
-        boolean[][] dp = new boolean[n][(totSum/2)+1];
+        boolean[] prev = new boolean[(totSum/2)+1];
         for(int i=0;i<n;i++){
-            dp[i][0] = true;
+            prev[0] = true;
         }
         if(nums[0] <= totSum/2)
-            dp[0][nums[0]] = true;
+            prev[nums[0]] = true;
         for(int ind=1;ind<n;ind++){
+            boolean curr[] = new boolean[(totSum/2)+1];
             for(int target=1;target<=totSum/2;target++){
-                boolean notTake = dp[ind-1][target];
+                boolean notTake = prev[target];
                 boolean take = false;
                 if(nums[ind] <= target)
-                    take = dp[ind-1][target-nums[ind]];
-                dp[ind][target] = (take || notTake);
+                    take = prev[target-nums[ind]];
+                curr[target] = (take || notTake);
             }
+            prev = curr;
         }
-        return dp[n-1][totSum/2];
-    }
-    boolean solve(int ind,int target,int[] nums,int[][] dp){
-        if(target == 0)
-            return false;
-        if(ind == 0)
-            return nums[0] == target;
-        if(dp[ind][target]!=-1)
-            return dp[ind][target]==1;
-        boolean notTake = solve(ind-1,target,nums,dp);
-        boolean take = false;
-        if(nums[ind] <= target)
-            take = solve(ind-1,target-nums[ind],nums,dp);
-        dp[ind][target] = (take || notTake)?1:0;
-        return take || notTake;
+        return prev[totSum/2];
     }
 }
