9122d7ba2745d53498f16db3b635dda112e90132
Rohith1972 <rohith197277@gmail.com>
2026-07-18 17:28:39 +0530

    Time: 3 ms (90.20%) | Memory: 45.1 MB (39.70%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,13 +1,33 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return solve(nums.length-1,0,target,nums);
+        int totSum = 0;
+        for(int x : nums)
+            totSum += x;
+        if ((target) > totSum)
+            return 0;
+
+        if ((totSum - target) % 2 != 0)
+            return 0;
+        int sum = (totSum - target) / 2;
+        int[][] dp = new int[nums.length][sum+1];
+        for(int[] i : dp)
+            Arrays.fill(i,-1);
+        return solve(nums.length-1,sum,nums,dp);
     }
-    static int solve(int ind,int ans,int target,int[] nums){
-        if(ind < 0){
-            return (ans == target)?1:0;
+    static int solve(int ind,int target,int[] nums,int[][] dp){
+        if(ind == 0){
+            if(target == 0 && nums[0] == 0)
+                return 2;
+            if(target == 0 || nums[0] == target)
+                return 1;
+            return 0;
         }
-        int add = solve(ind-1,ans+nums[ind],target,nums);
-        int sub = solve(ind-1,ans-nums[ind],target,nums);
-        return add+sub;
+        if(dp[ind][target]!=-1)
+            return dp[ind][target];
+        int  notTake = solve(ind-1,target,nums,dp);
+        int  take = 0;
+        if(nums[ind] <= target)
+            take = solve(ind-1,target-nums[ind],nums,dp);
+        return dp[ind][target] = (take+notTake);
     }
 }
