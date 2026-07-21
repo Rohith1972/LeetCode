4ccf12a9323dcbeb53e74c759472b3023e4897dd
Rohith1972 <rohith197277@gmail.com>
2026-07-21 22:54:37 +0530

    Time: 21 ms (54.70%) | Memory: 46.5 MB (94.40%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,20 +1,19 @@
 class Solution {
     public int longestCommonSubsequence(String s1, String s2) {
-        int[][] dp = new int[s1.length()+1][s2.length()+1];
+        int[] prev = new int[s2.length()+1];
         for(int i=0;i<s2.length();i++){
-            dp[0][i] = 0;
-        }
-        for(int i=0;i<s1.length();i++){
-            dp[i][0] = 0;
+            prev[i] = 0;
         }
         for(int ind1=1;ind1<=s1.length();ind1++){
+            int[] curr = new int[s2.length()+1];
             for(int ind2=1;ind2<=s2.length();ind2++){
                 if(s1.charAt(ind1-1) == s2.charAt(ind2-1))
-                    dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
-                else dp[ind1][ind2] = Math.max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+                    curr[ind2] = 1 + prev[ind2-1];
+                else curr[ind2] = Math.max(prev[ind2],curr[ind2-1]);
             }
+            prev = curr;
         }
-        return dp[s1.length()][s2.length()];
+        return prev[s2.length()];
     }
     int solve(int ind1,int ind2,String s1,String s2,int[][] dp){
         if(ind1<0 || ind2<0)
