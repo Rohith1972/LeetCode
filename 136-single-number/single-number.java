2bf92c3f4bf15b44bac5852fa5635b8f06d9b3cd
Rohith1972 <rohith197277@gmail.com>
2026-08-17 20:47:19 +0530

    Time: 1 ms (99.94%) | Memory: 47.2 MB (13.86%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,20 +1,9 @@
 class Solution {
-    public int singleNumber(int[] arr) {
-        LinkedHashMap<Integer,Integer> hm = new LinkedHashMap<>();
-        for(int i=0;i<arr.length;i++){
-            if(!hm.containsKey(arr[i]))
-                hm.put(arr[i],1);
-            else
-                hm.put(arr[i],hm.get(arr[i])+1);
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        for(int i=0;i<nums.length;i++){
+            ans = ans ^ nums[i];
         }
-        Set<Integer> st = hm.keySet();
-        int res = 0;
-        for(Integer i : st){
-            if(hm.get(i)==1){
-                res=i;
-                break;
-            }
-        }
-        return res;
+        return ans;
     }
 }
